5ae3bbbc924231b262b4e59139b9963c10b3aa46
BrdML <bert.m310@gmail.com>
2024-04-27 12:00:56 +0200

    first commit

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+    <div>
+        <div className="menu-burger">
+
+        </div>
+        <div className="div">
+            
+        </div>
+    </div>
+  )
+}
+
+export default NavBar
